ab4a3f371fa9dfd24804ec6f805a2d8b3e962cf8
lukasmartinelli <me@lukasmartinelli.ch>
2016-09-17 22:12:49 +0200

    Tweak chat bubbles

--- src/chat/message.jsx
+++ src/chat/message.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import moment from 'moment'
+import styles from '../index.scss'
 
 // A single message from an author at a given time
 class Message extends React.Component {
@@ -12,19 +13,11 @@ class Message extends React.Component {
   render() {
     const timeAgo = moment(this.props.timestamp).fromNow()
     return <div>
-      <div style={{paddingBottom: 5, fontSize: 12}}>
-        <span>{this.props.author}</span>
-        <span style={{color: '#a8aab1', paddingLeft: 6}}>{timeAgo}</span>
+      <div className={styles.messageHeader}>
+        <span className={styles.messageAuthor}>{this.props.author}</span>
+        <span className={styles.messageDate}>{timeAgo}</span>
       </div>
-      <div style={{
-				padding: 7,
-				backgroundColor: '#86BB71',
-				color: 'white',
-				borderRadius: 7,
-				width: '90%',
-				marginBottom: 10,
-				lineHeight: '26px',
-			}}>
+      <div className={styles.messageContent}>
         {this.props.message}
       </div>
     </div>
