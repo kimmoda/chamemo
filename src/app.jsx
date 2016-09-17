e08b7a0b67be72d8ae62b92d0f5ef41b7e15d667
lukasmartinelli <me@lukasmartinelli.ch>
2016-09-17 15:06:31 +0200

    Add box shadow

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-import Drawer from 'rebass/dist/Drawer'
-import Container from 'rebass/dist/Container'
-import Block from 'rebass/dist/Block'
-import Fixed from 'rebass/dist/Fixed'
 import { MessageBox } from './messagebox'
 import firebase from 'firebase'
 
@@ -10,39 +6,22 @@ import { Map } from './map.jsx'
 import { DoodleList } from './doodlelist'
 
 import theme from './theme.js'
+import { fullHeight } from './theme.js'
 
 
 export default class App extends React.Component {
-  static childContextTypes = {
-    rebass: React.PropTypes.object,
-    reactIconBase: React.PropTypes.object
-  }
-
-  constructor(props) {
-    super(props)
-  }
-
-  getChildContext() {
-    return {
-      rebass: theme,
-      reactIconBase: {size: 20}
-    }
-  }
-
   render() {
     return <div style={{ fontFamily: theme.fontFamily, color: theme.color, fontWeight: 300 }}>
-      <DoodleList />
       <div style={{
-      position: 'absolute',
-      backgroundColor: 'white',
+      ...fullHeight,
+      boxShadow: '#ddd 2px 2px 8px 0px',
+      backgroundColor: '#f4f7f8',
       zIndex: 1000,
       left: 0,
-      right: 0,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      width: 500,
-      height: 300}}>
+      width: 300,
+      padding: 20 }}>
         <MessageBox />
+        <DoodleList />
       </div>
       <Map onPitchClick={pitch => console.log('Pitch clicked', pitch)}/>
     </div>
