c200f0f1e4fd32d3b48493fdc7dea96b2756ce5d
lukasmartinelli <me@lukasmartinelli.ch>
2016-09-17 11:19:04 +0200

    Query source features in map

--- src/map.jsx
+++ src/map.jsx
@@ -2,11 +2,32 @@ import React from 'react'
 import MapboxGl from 'mapbox-gl';
 import { fullHeight } from './theme.js'
 
+function findSportAreas(map) {
+  return map.querySourceFeatures('composite', {
+    sourceLayer: 'landuse',
+    filter: ['in', 'class', 'pitch']
+  })
+}
+
 export class Map extends React.Component {
 	static propTypes = {
 		mapStyle: React.PropTypes.string.isRequired,
 		accessToken: React.PropTypes.string,
 	}
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      sportAreas: []
+    }
+    this.updateSportAreas = this.updateSportAreas.bind(this)
+  }
+
+  updateSportAreas() {
+      this.setState({
+        sportAreas: findSportAreas(this.state.map)
+      })
+  }
 
 	componentDidMount() {
 		MapboxGl.accessToken = this.props.accessToken
@@ -14,7 +35,14 @@ export class Map extends React.Component {
 		const map = new MapboxGl.Map({
 			container: this.container,
 			style: this.props.mapStyle,
+      zoom: 17,
+      hash: true,
+      center: [9.02728, 47.21423],
 		});
+
+    map.on('moveend', this.updateSportAreas)
+    map.on('dragend', this.updateSportAreas)
+    map.on('zoomend', this.updateSportAreas)
 
 		map.on("style.load", (...args) => {
 			this.setState({ map });
@@ -22,6 +50,7 @@ export class Map extends React.Component {
 	}
 
 	render() {
+      console.log(this.state.sportAreas)
 			return <div
 				ref={x => this.container = x}
 				style={{
