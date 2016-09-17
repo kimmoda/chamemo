ac8b069ef68d6527e339c51670792ed5675f503d
aqulu <akku@outlook.com>
2016-09-17 13:47:04 +0200

    cors stuff

--- src/doodlelist.jsx
+++ src/doodlelist.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { DoodlePreview } from './doodlepreview'
 import { DoodleForm } from './doodleform'
+import client from './client'
+import minixhr from 'minixhr'
+
+const pollsurl = 'https://api.tamedia.cloud/doodle/v1/polls'
 
 export class DoodleList extends React.Component {
   constructor(props) {
@@ -16,9 +20,34 @@ export class DoodleList extends React.Component {
 
 	onNewDoodle(title, date) {
 		const doodles = this.state.doodles
-		doodles.push({id: (Math.random() * 16).toString(16), title: title, date: date})
-		this.setState({
-			doodles: doodles
+		const entity = {
+			"options": [
+				{ "start": date }
+			],
+			"type": "DATE",
+			"title": title,
+			"initiator": {
+				"name": "Jim Knopf",
+				"email": "h347031@mvrht.com"
+			}
+		}
+
+		minixhr({ url: pollsurl, method: 'OPTIONS', headers: {'apikey': '6a0d6c3dbfbd443aa5fea58c4b612c5b' } }, () => {
+			const req = {
+				url: pollsurl,
+				method: 'POST',
+				data: JSON.stringify(entity),
+				headers: {'Content-Type': 'application/json', 'Accept': 'application/json', 'apikey': '6a0d6c3dbfbd443aa5fea58c4b612c5b' }
+			}
+
+			minixhr(req, response => {
+				console.log(response)
+				doodles.push({ id: response.id, title: response.title, date: response.options[0].start })
+				this.setState({
+					doodles: doodles
+				})
+			})
+
 		})
 	}
 
